2bc8c65be014f457db0cccc1083a3fdeac37745d
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-11 20:33:45 -0300

    Add files via upload

--- src/main/java/IMC/IMC/Controller/C_IMC.java
+++ src/main/java/IMC/IMC/Controller/C_IMC.java
@@ -2,24 +2,29 @@ package IMC.IMC.Controller;
 
 import IMC.IMC.Service.S_IMC;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class C_IMC {
 
-    @GetMapping("/")
-    public String paginaInicial(){
-        return "Home/home";
+    private final S_IMC s_imc;
+
+    public C_IMC(S_IMC s_imc) {
+        this.s_imc = s_imc;
     }
 
     @PostMapping("/")
-    public String calculadoraImc(@RequestParam("altura") long altura,
-                                 @RequestParam("peso") long peso) {
+    public String calculadoraImc(@RequestParam("altura") float altura,
+                                 @RequestParam("peso") float peso,
+                                 Model model) {
+
+        s_imc.configImc(altura, peso);
+        float imc = s_imc.calcImc();
+
+        model.addAttribute("resultadoImc", imc);
+
         return "Home/home";
     }
-
-    S_IMC s_imc = new S_IMC();
-
 }
--- src/main/java/IMC/IMC/Service/S_IMC.java
+++ src/main/java/IMC/IMC/Service/S_IMC.java
@@ -5,6 +5,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class S_IMC {
 
-    
+    private float altura;
+    private float peso;
+
+    public void configImc(float altura, float peso) {
+        altura = this.altura;
+        peso = this.peso;
+    }
+
+    public float calcImc() {
+        return peso / (altura * altura);
+    }
 
 }
